09975aed8b07fdd6c74a8d20af1124057c9ee913
przemek83 <4788832+przemek83@users.noreply.github.com>
2025-04-16 19:51:31 +0200

    Added skeleton project.

---
+++ src/DataExplorer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+public class Calculator
+{
+  public double Add(double a, double b)
+  {
+    return a + b;
+  }
+}
+
+namespace DataExplorer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+            double result = calc.Add(2, 3);
+            Console.WriteLine($"The result is: {result}");
+        }
+    }
+}
