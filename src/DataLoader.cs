0e7e073b5c577851f3da3891ed6e0992bdd89e4e
Przemek <4788832+przemek83@users.noreply.github.com>
2025-04-20 17:49:21 +0200

    Create DataLoader.cs
    Add DataLoader interface.

---
+++ src/DataLoader.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DataExplorer
+{
+    public interface IDataLoader
+    {
+        public abstract bool Load();
+
+        public abstract string[] GetHeaders();
+
+        public abstract ColumnType[] GetColumnTypes();
+
+        public abstract string[,] GetData();
+    }
+}
