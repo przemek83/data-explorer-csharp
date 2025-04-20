1aa06244c88ad1a992cf3c6875a2ec63ac8de681
Przemek <4788832+przemek83@users.noreply.github.com>
2025-04-20 18:41:19 +0200

    Create FileDataLoader.cs
    Add FileDataLoader with implementation missing.

---
+++ src/FileDataLoader.cs
@@ -0,0 +1,25 @@
+﻿namespace DataExplorer
+{
+    internal class FileDataLoader : IDataLoader
+    {
+        public ColumnType[] GetColumnTypes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string[,] GetData()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string[] GetHeaders()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Load()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
