94ea481481ebbe10bab7a7c5a8f83e7c80017c23
Przemek <4788832+przemek83@users.noreply.github.com>
2025-04-20 18:36:36 +0200

    Create Dataset.cs
    Add Dataset class.

---
+++ src/Dataset.cs
@@ -0,0 +1,38 @@
+﻿namespace DataExplorer
+{
+    public class Dataset
+    {
+        public Dataset()
+        {
+        }
+
+        public bool Initialize(string file)
+        {
+            return false;
+        }
+
+        public (bool, int) ColumnNameToId(string columnName)
+        {
+            return (false, -1);
+        }
+
+        public (bool, string) ColumnIdToName(int columnId)
+        {
+            return (false, "");
+        }
+
+        public (bool, ColumnType) GetColumnType(int columnId)
+        {
+            return (false, ColumnType.UNKNOWN);
+        }
+
+        public (bool, List<dynamic>) GetColumnData(int columnId)
+        {
+            return (false, new List<dynamic>());
+        }
+
+        readonly private string[] headers_;
+        readonly private ColumnType[] columnTypes_;
+        readonly private string[,] data_;
+    }
+}
