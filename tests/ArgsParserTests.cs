c2806fcd5491308fe3a11005121e585958e5976a
Przemek <4788832+przemek83@users.noreply.github.com>
2025-04-20 16:14:29 +0200

    Create ArgsParserTests.cs
    Add tests for ArgParser class.

---
+++ tests/ArgsParserTests.cs
@@ -0,0 +1,58 @@
+using System.CommandLine;
+using System.CommandLine.Parsing;
+using System.Runtime.Intrinsics.X86;
+using Xunit;
+using static System.Formats.Asn1.AsnWriter;
+
+namespace DataExplorer.Tests
+{
+    public class ArgsParserTests
+    {
+        [Fact]
+        public void IsValid_ShouldReturnTrue_WhenArgumentsAreValid()
+        {
+            string[] args = new[] { "sample.txt", "avg", "score", "first_name" };
+            ArgsParser parser = new ArgsParser(args);
+
+            var result = parser.IsValid();
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void IsValid_ShouldReturnFalse_WhenOperationIsInvalid()
+        {
+            string[] args = new[] { "sample.txt", "invalid", "score", "first_name" };
+            ArgsParser parser = new ArgsParser(args);
+
+            bool result = parser.IsValid();
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void IsValid_ShouldReturnFalse_WhenNotEnoughArgsPassed()
+        {
+            string[] args = new[] { "sample.txt", "invalid", "score" };
+            ArgsParser parser = new ArgsParser(args);
+
+            bool result = parser.IsValid();
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void GetResults_ShouldReturnCorrectParsedValues_WhenArgumentsAreValid()
+        {
+            string[] args = new[] { "sample.txt", "sum", "score", "first_name" };
+            ArgsParser parser = new ArgsParser(args);
+
+            var (file, operation, aggregation, grouping) = parser.GetResults();
+
+            Assert.Equal("sample.txt", file);
+            Assert.Equal("sum", operation);
+            Assert.Equal("score", aggregation);
+            Assert.Equal("first_name", grouping);
+        }
+    }
+}
