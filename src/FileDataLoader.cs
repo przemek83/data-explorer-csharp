2e6d41eecfb22923c63037872eeeb0791656a13e
Przemek <4788832+przemek83@users.noreply.github.com>
2025-04-21 14:29:29 +0200

    Fill some logic in FileDataLoader
    Add some logic into methods from FileDataLoader class.

--- src/FileDataLoader.cs
+++ src/FileDataLoader.cs
@@ -19,14 +19,53 @@
 
         public string[] GetHeaders()
         {
-            throw new NotImplementedException();
+            return headers_;
         }
 
         public bool Load()
         {
-            throw new NotImplementedException();
+            StreamReader reader = new StreamReader(stream_);
+            if (reader.EndOfStream)
+                return false;
+
+            headers_ = processHeadersLine(reader.ReadLine() ?? "");
+            
+            if (reader.EndOfStream)
+                return false;
+
+            //columns_ = processColumnTypesLine(reader.ReadLine() ?? "");
+
+            return true;
+        }
+
+        private string[] processHeadersLine(string line)
+        {
+            string[] headers = line.Split(';');
+            for (int i = 0; i < headers.Length; i++)
+                headers[i] = headers[i].Trim();
+            return headers;
+        }
+
+        private IColumn[] processColumnTypesLine(string line)
+        {
+            string[] columnTypeNames = line.Split(';');
+            IColumn[] columns = new IColumn[columnTypeNames.Length];
+            for (int i = 0; i < columnTypeNames.Length; i++)
+            {
+                ColumnType type = ColumnTypeExtensions.StringToColumnType(columnTypeNames[i]);
+                columns[i] = type switch
+                {
+                    ColumnType.INTEGER => new ColumnNumeric(),
+                    ColumnType.STRING => new ColumnString(),
+                    _ => throw new NotImplementedException(),
+                };
+            }
+
+            return columns;
         }
 
         private readonly Stream stream_;
+        private string[] headers_;
+        private IColumn[] columns_;
     }
 }
