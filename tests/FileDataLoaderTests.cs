ad83d032c3c4ba047044646d42cc3646ba9eaca4
Przemek <4788832+przemek83@users.noreply.github.com>
2025-04-21 14:29:46 +0200

    Update FileDataLoaderTests.cs
    Correct test data.

--- tests/FileDataLoaderTests.cs
+++ tests/FileDataLoaderTests.cs
@@ -95,13 +95,13 @@ namespace Tests
         {
             string sampleData = """
 first_name;age;movie_name;score
-string; integer; string; integer
-tim; 26; inception; 8
-tim; 26; pulp_fiction; 8
-tamas; 44; inception; 7
-tamas; 44; pulp_fiction; 4
-dave; 0; inception; 8
-dave; 0; ender's_game;8
+string;integer;string;integer
+tim;26;inception;8
+tim;26;pulp_fiction;8
+tamas;44;inception;7
+tamas;44;pulp_fiction;4
+dave;0;inception;8
+dave;0;ender's_game;8
 """;
 
            return new MemoryStream(Encoding.UTF8.GetBytes(sampleData));
