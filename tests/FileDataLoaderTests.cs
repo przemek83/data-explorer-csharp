f073b9e9d21d40d0adf172136a60bc188f3b33cc
Przemek <4788832+przemek83@users.noreply.github.com>
2025-04-21 10:59:44 +0200

    Create FileDataLoaderTests.cs
    Add first (failing for now) test for data loading.

---
+++ tests/FileDataLoaderTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+using DataExplorer;
+using Newtonsoft.Json.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Tests
+{
+    public class FileDataLoaderTests
+    {
+
+        [Fact]
+        public void Load_ShouldReturnTrue_WhenProperDataLoaded()
+        {
+            FileDataLoader loader = new FileDataLoader(GetValidStream());
+            Assert.True(loader.Load());
+        }
+
+        Stream GetValidStream()
+        {
+            string sampleData = """
+first_name;age;movie_name;score
+string; integer; string; integer
+tim; 26; inception; 8
+tim; 26; pulp_fiction; 8
+tamas; 44; inception; 7
+tamas; 44; pulp_fiction; 4
+dave; 0; inception; 8
+dave; 0; ender's_game;8
+""";
+
+           return new MemoryStream(Encoding.UTF8.GetBytes(sampleData));
+        }
+    }
+}
