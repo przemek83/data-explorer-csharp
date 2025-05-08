b2d6011532ea461af04aff9302578b103a76924a
Przemek <4788832+przemek83@users.noreply.github.com>
2025-05-08 19:51:53 +0200

    Add Query class
    Add Query class and Operation::Type enum.

--- src/Operation.cs
+++ src/Operation.cs
@@ -2,6 +2,14 @@
 {
     public class Operation
     {
+        public enum Type : byte
+        {
+            AVG = 0,
+            MAX = 1,
+            MIN = 2,
+            UNKNOW = 3
+        }
+
         public static string[] Allowed() { return ["avg", "min", "max"]; }
     }
 }
---
+++ src/Query.cs
@@ -0,0 +1,9 @@
+﻿namespace DataExplorer
+{
+    public readonly struct Query(int aggregateColumnID, int groupingColumnID, Operation.Type type)
+    {
+        public readonly int AggregateColumnID { get; } = aggregateColumnID;
+        public readonly int GroupingColumnID { get; } = groupingColumnID;
+        public readonly Operation.Type Type { get; } = type;
+    }
+}
