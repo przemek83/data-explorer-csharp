b65bb5d75a522acfd62d52ec7ddccd4a845fa4bc
Przemek <4788832+przemek83@users.noreply.github.com>
2025-05-13 10:17:12 +0200

    Add ToType method into Operation class
    Add ToType method into Operation class. Add tests.

--- src/Operation.cs
+++ src/Operation.cs
@@ -1,4 +1,6 @@
-﻿namespace DataExplorer
+﻿using System.CommandLine.Parsing;
+
+namespace DataExplorer
 {
     public class Operation
     {
@@ -11,5 +13,16 @@
         }
 
         public static string[] Allowed() { return ["avg", "min", "max"]; }
+
+        public static Type ToType(string name)
+        {
+            return name.ToLower() switch
+            {
+                "avg" => Type.AVG,
+                "min" => Type.MIN,
+                "max" => Type.MAX,
+                _ => Type.UNKNOW,
+            };
+        }
     }
 }
---
+++ tests/OperationTests.cs
@@ -0,0 +1,37 @@
+namespace DataExplorer.Tests
+{
+    public class OperationTests
+    {
+        [Theory]
+        [InlineData("avg", Operation.Type.AVG)]
+        [InlineData("min", Operation.Type.MIN)]
+        [InlineData("max", Operation.Type.MAX)]
+        [InlineData("unknown", Operation.Type.UNKNOW)]
+        [InlineData("invalid", Operation.Type.UNKNOW)]
+        [InlineData("", Operation.Type.UNKNOW)]
+        public void ToType_ShouldReturnCorrectType(string input, Operation.Type expected)
+        {
+            // Arrange
+            var operation = new Operation();
+
+            // Act
+            var result = Operation.ToType(input);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Allowed_ShouldReturnExpectedValues()
+        {
+            // Arrange
+            var expected = new[] { "avg", "min", "max" };
+
+            // Act
+            var result = Operation.Allowed();
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
